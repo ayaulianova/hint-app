a97fb075a3ae02c19cb28cfa3534d03d4cb5f2d9
ayaulianova <alphabet_mist@outlook.com>
2020-10-29 08:53:31 +0300

    add list of patterns

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-
+import React from 'react';
 import './App.css';
+
+const PATTERNS = [
+  "Abstract Factory",
+  "Builder",
+  "Factory Method",
+  "Prototype",
+  "Singleton",
+  "Adapter",
+  "Bridge",
+  "Composite",
+  "Decorator",
+  "Facade",
+  "Flyweight",
+  "Proxy",
+  "Chain of responsibility",
+  "Command",
+  "Interpreter",
+  "Iterator",
+  "Mediator",
+  "Memento",
+  "Observer",
+  "State",
+  "Strategy",
+  "Template method",
+  "Visitor",
+];
 
 function App() {
   return (
     <div className="App">
-        
+      <ul>
+        {PATTERNS.map((i, index) => <li key={index}>{i.toString()}</li>)}
+      </ul>
     </div>
   );
 }
