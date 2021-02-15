93357587b4d917042551d912e08e63ed4b16e69a
ayaulianova <alphabet_mist@outlook.com>
2021-02-15 22:42:34 +0300

    renaming

--- src/containers/List/List.js
+++ src/containers/List/List.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import listOfPatterns from './constants/listOfPatterns'
-import './List.scss'
+import './List.css'
 
 export default function List() {
     const [active, setActive] = useState(0)
