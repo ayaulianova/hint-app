ea0a6b3624341b89a8b5bcdb2e089e31188fd574
ayaulianova <alphabet_mist@outlook.com>
2021-02-28 23:51:24 +0300

    merge

--- src/components/Pattern/Pattern.js
+++ src/components/Pattern/Pattern.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-<<<<<<< HEAD
 import './Pattern.css'
 
 export default function Pattern(props) {
@@ -8,14 +7,6 @@ export default function Pattern(props) {
     <li>
       {props.value}  
     </li>
-=======
-
-export default function Pattern(props) {
-  return(
-    <div>
-      {props.value}  
-    </div>
->>>>>>> 6cf622d81c13be5d9aa8b51277097afba072e8dd
   )
 }
 
